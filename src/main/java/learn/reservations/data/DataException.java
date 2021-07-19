97d0938e599302c99d18a9a5768e033fe0bf40d0
ymasog <yer.masog@gmail.com>
2021-07-19 14:49:04 -0500

    created dataException

---
+++ src/main/java/learn/reservations/data/DataException.java
@@ -0,0 +1,8 @@
+package learn.reservations.data;
+
+public class DataException extends Exception{
+
+    public DataException(String message) {super(message); }
+
+
+}
--- src/main/java/learn/reservations/data/ReservationRepository.java
+++ src/main/java/learn/reservations/data/ReservationRepository.java
@@ -1,4 +1,11 @@
 package learn.reservations.data;
 
+import learn.reservations.models.Reservation;
+
+import java.util.List;
+
 public interface ReservationRepository {
+    List<Reservation> findByEmail(String email);
+
+
 }
