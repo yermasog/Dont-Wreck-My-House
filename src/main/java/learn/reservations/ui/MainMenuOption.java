fc981ef76a3da71898605655aa083b2e26748e36
ymasog <yer.masog@gmail.com>
2021-07-19 14:40:29 -0500

    main menu option enum created

---
+++ src/main/java/learn/reservations/ui/MainMenuOption.java
@@ -0,0 +1,27 @@
+package learn.reservations.ui;
+
+public enum MainMenuOption {
+    EXIT(0, "Exit"),
+    VIEW_RES_BY_HOST(1, "View Reservations by Host"),
+    MAKE_RESERVATION(2, "Make Reservation"),
+    EDIT_RESERVATION(3, "Edit Reservation"),
+    CANCEL_RESERVATION(4, "Cancel Reservation");
+
+    private int value;
+    private String message;
+
+    private MainMenuOption(int value, String message) {
+        this.value = value;
+        this.message = message;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+   //TODO write method to return enum based on value
+}
